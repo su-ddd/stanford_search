6da919e0b546bd9755ef570394418d5b12a83bf1
Marco Wise <marco.wise@stanford.edu>
2014-07-10 14:15:26 -0700

    initial commit

---
+++ js/google_cse.js
@@ -0,0 +1,7 @@
+jQuery(document).ready(function() {
+    var cx = Drupal.settings.stanford_search.id;
+    var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
+    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+        '//www.google.com/cse/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
+  });
