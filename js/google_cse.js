0551a2a47676ec4ba980ec3684be012cd9034f84
Marco Wise <marco.wise@stanford.edu>
2015-09-16 13:54:36 -0700

    updated Google CSE URL

--- js/google_cse.js
+++ js/google_cse.js
@@ -2,6 +2,6 @@ jQuery(document).ready(function() {
     var cx = Drupal.settings.stanford_search.id;
     var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
     gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-        '//www.google.com/cse/cse.js?cx=' + cx;
+        '//cse.google.com/cse.js?cx=' + cx;
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
   });
